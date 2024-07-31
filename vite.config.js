2bc224d453f39c0b6cf99fb57a7e1da285c5a80e
EdgarDCast <edgardavid21214@gmail.com>
2024-07-31 15:14:00 -0600

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,60 +1,59 @@
-import { defineConfig } from "vite";
-import * as glob from "glob";
+import { defineConfig } from 'vite';
+import * as glob from 'glob';
+import path, { resolve } from 'node:path';
 import { ViteMinifyPlugin } from 'vite-plugin-minify';
 import htmlPurge from 'vite-plugin-purgecss';
-import path, { resolve } from "node:path";
 import handlebars from 'vite-plugin-handlebars';
+import handlerBarsContext from './variables.js';
 
-export default defineConfig(
-    {
-        appType: "mpa",
-        base: "/pw2_vitempa/",
-        build: {
-            rollupOptions: {
-                input: Object.fromEntries(
-                    [
-                        // Genera entradas para todos los archivos HTML excepto los del directorio 'dist'
-                        ...glob.sync('./!(dist)/*.html').map(file => [
-                            file.slice(0, file.length - path.extname(file).length), resolve(__dirname, file)
-                        ]),
-                        // Genera entradas para todos los archivos HTML en la raíz del proyecto
-                        ...glob.sync('./*.html').map(file => [
-                            file.slice(0, file.length - path.extname(file).length), resolve(__dirname, file)
-                        ])
-                    ]
-                ),
-            },
+export default defineConfig({
+    base: "/pw2_vitempa/", // Base URL para el proyecto, útil para despliegue
+    appType: 'mpa', // Multi-page application (MPA) para manejar múltiples entradas HTML
+    build: {
+        rollupOptions: {
+            input: Object.fromEntries(
+                [
+                    // Genera entradas para todos los archivos HTML excepto los del directorio 'dist'
+                    ...glob.sync('./!(dist)/*.html').map(file => [
+                        file.slice(0, file.length - path.extname(file).length), resolve(__dirname, file)
+                    ]),
+                    // Genera entradas para todos los archivos HTML en la raíz del proyecto
+                    ...glob.sync('./*.html').map(file => [
+                        file.slice(0, file.length - path.extname(file).length), resolve(__dirname, file)
+                    ])
+                ]
+            ),
         },
-        plugins: [
-            handlebars({
-                partialDirectory: resolve(__dirname, 'partials'), // Directorio de plantillas parciales de Handlebars
-                context: handlerBarsContext, // Contexto para las plantillas de Handlebars
-                order: 'pre', // Define el orden de ejecución de los plugins
-                handler: (content) => { // Personalización del contenido, si es necesario
-                    // Código de transformación, actualmente no modifica el contenido
-                    return content;
-                },
-                helpers: {
-                    // Helper personalizado para filtrar imágenes por tipo
-                    filtrarPorTipo: function (imagenes, tipo, options) {
-                        var result = '';
-                        imagenes.forEach(function (imagen) {
-                            if (imagen.tipo === tipo) {
-                                result += options.fn(imagen);
-                            }
-                        });
-                        return result;
-                    }
+    },
+    plugins: [
+        handlebars({
+            partialDirectory: resolve(__dirname, 'partials'), // Directorio de plantillas parciales de Handlebars
+            context: handlerBarsContext, // Contexto para las plantillas de Handlebars
+            order: 'pre', // Define el orden de ejecución de los plugins
+            handler: (content) => { // Personalización del contenido, si es necesario
+                // Código de transformación, actualmente no modifica el contenido
+                return content;
+            },
+            helpers: {
+                // Helper personalizado para filtrar imágenes por tipo
+                filtrarPorTipo: function (imagenes, tipo, options) {
+                    var result = '';
+                    imagenes.forEach(function (imagen) {
+                        if (imagen.tipo === tipo) {
+                            result += options.fn(imagen);
+                        }
+                    });
+                    return result;
                 }
-            }),
-            htmlPurge({
-                // Opciones para purgar CSS no utilizado
-                content: ['./src/**/*.html'], // Archivos HTML a analizar para purga de CSS
-                safelist: ['safe-class'], // Clases CSS que deben mantenerse
-            }),
-            ViteMinifyPlugin({
-                // Opciones para la minificación de JavaScript y CSS
-            }),
-        ],
-    });
-    
+            }
+        }),
+        htmlPurge({
+            // Opciones para purgar CSS no utilizado
+            content: ['./src/**/*.html'], // Archivos HTML a analizar para purga de CSS
+            safelist: ['safe-class'], // Clases CSS que deben mantenerse
+        }),
+        ViteMinifyPlugin({
+            // Opciones para la minificación de JavaScript y CSS
+        }),
+    ],
+});
